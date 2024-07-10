23474ca9378614f570b9fc2e4142583eed0e653d
Marat Ayaibergenov <marataya@gmail.com>
2024-07-10 23:46:54 +0500

    Add todo app, create empty component template and model

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { TodoList } from './todoList';
+import { TodoItem } from './todoItem';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+  ]);
+
+  get username(): string {
+    return this.list.user
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length;
+  }
+}
