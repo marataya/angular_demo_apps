36c5f4ae131c4b89097b24cfb530184e1aadb62a
Marat Ayaibergenov <marataya@gmail.com>
2024-07-14 12:37:59 +0500

    add pagination to product listion in store.component

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -9,7 +9,11 @@ import { ProductRepository } from "../model/product.repository";
 export class StoreComponent {
     products: Signal<Product[]>;
     categories: Signal<string[]>;
-    selectedCategory = signal<string|undefined>(undefined);
+    selectedCategory = signal<string | undefined>(undefined);
+    productsPerPage = signal(4);
+    selectedPage = signal(1);
+    pagedProducts: Signal<Product[]>;
+    pageNumbers: Signal<number[]>;
 
     constructor(private repository: ProductRepository) {
         this.products = computed(() => {
@@ -21,9 +25,27 @@ export class StoreComponent {
             }
         })
         this.categories = repository.categories;
+        let pageIndex = computed(() => {
+            return (this.selectedPage() - 1) * this.productsPerPage()
+        });
+        this.pagedProducts = computed(() => {
+            return this.products().slice(pageIndex(), pageIndex() + this.productsPerPage());
+        });
+        this.pageNumbers = computed(() => {
+            return Array(Math.ceil(this.products().length / this.productsPerPage()))
+                .fill(0).map((x,i) => i+1);
+        });
     }
 
     changeCategory(newCategory?: string) {
         this.selectedCategory.set(newCategory);
+        this.changePage(1);
+    }
+    changePage(newPage: number) {
+        this.selectedPage.set(newPage);
+    }
+    changePageSize(newSize: number) {
+        this.productsPerPage.set(Number(newSize));
+        this.changePage(1);
     }
 }
